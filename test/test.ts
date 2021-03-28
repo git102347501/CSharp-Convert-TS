f96cf54f84b3b236fdf19d3bbdbb395741bb4f62
赵强强 <zhaoqiangqiang@senruisoft.com>
2021-03-28 17:15:18 +0800

    fix(0.1.27) 修复备注读取错误问题，修复属性重复问题

---
+++ test/test.ts
@@ -0,0 +1,26 @@
+/// <summary>
+    /// 医院设备信息
+    /// </summary>
+    public class HospitalDevice : CreationAuditedEntity<Guid>
+    {
+        /// <summary>
+        /// 医院ID
+        /// </summary>
+        public Guid HospitalId { get; set; }
+
+        /// <summary>
+        /// 设备品牌
+        /// </summary>
+        public string Brand { get; set; }
+
+        /// <summary>
+        /// 设备型号
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// 设备数量
+        /// </summary>
+        public int Amount { get; set; }
+
+    }
